ed210c442942c8e2b17176b68c30949899fdde25
Nit-Harley <harleynit63@gmail.com>
2024-02-19 02:04:08 +0100

    modification fichier photo

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,10 +17,10 @@ import decapCmsOauth from "astro-decap-cms-oauth";
 
 // https://astro.build/config
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
-  output: 'server',
-  site: 'https://crypto-afrique-futur.netlify.app', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  output: 'hybrid',
+  site: 'https://crypyohub.vercel.app', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   server: {
-    // port: 4321, // The port to run the dev server on.
+     port: 4321, // The port to run the dev server on.
   },
   markdown: {
     syntaxHighlight: 'shiki',
@@ -52,5 +52,5 @@ export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
       allowNodeBuiltins: true
     }
   },
-  adapter: vercel()
+ 
 });
